cbf5c6e60d25abbc70cbe4d3645539760014e560
sulta <sultandjavgashev@gmail.com>
2023-08-11 14:00:13 +0500

    fix bugs

--- module-repository/src/main/java/com/mjc/school/repository/utils/Utils.java
+++ module-repository/src/main/java/com/mjc/school/repository/utils/Utils.java
@@ -3,8 +3,7 @@ package com.mjc.school.repository.utils;
 import com.mjc.school.repository.model.impl.AuthorModel;
 import com.mjc.school.repository.model.impl.NewsModel;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,38 +13,44 @@ import java.util.Scanner;
 public class Utils {
     public static List<AuthorModel> readAuthors(String fileName) {
         List<AuthorModel> res = new ArrayList<>();
-        try (Scanner scanner = new Scanner(new File("module-repository/src/main/resources/" + fileName))) {
-            while (scanner.hasNextLine()) {
-                long id = res.size() + 1;
-                AuthorModel author = new AuthorModel();
-                author.setId(id);
-                author.setName(scanner.nextLine());
-                author.setCreatedDate(LocalDateTime.now());
-                res.add(author);
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        for (String el : readFile(fileName)) {
+            long id = res.size() + 1;
+            AuthorModel author = new AuthorModel();
+            author.setId(id);
+            author.setName(el);
+            author.setCreatedDate(LocalDateTime.now());
+            res.add(author);
         }
         return res;
     }
 
     public static List<NewsModel> readNews(String newsFile, String contentFile) {
         List<NewsModel> res = new ArrayList<>();
-        try (Scanner content = new Scanner(new File("module-repository/src/main/resources/" + contentFile)); Scanner news = new Scanner(new File("module-repository/src/main/resources/" + newsFile))) {
-            while (content.hasNextLine() && news.hasNextLine()) {
-                long id = res.size() + 1;
-                NewsModel newsModel = new NewsModel();
-                newsModel.setId(id);
-                newsModel.setCreateDate(LocalDateTime.now());
-                newsModel.setTitle(news.nextLine());
-                newsModel.setContent(content.nextLine());
-                Random rand = new Random();
-                newsModel.setAuthorId((long) 1 + rand.nextInt(31 - 1));
-                res.add(newsModel);
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        List<String> news = readFile(newsFile);
+        List<String> contents = readFile(contentFile);
+        for (int i = 0; i < news.size(); i++) {
+            long id = res.size() + 1;
+            NewsModel newsModel = new NewsModel();
+            newsModel.setId(id);
+            newsModel.setCreateDate(LocalDateTime.now());
+            newsModel.setTitle(news.get(i));
+            newsModel.setContent(contents.get(i));
+            Random rand = new Random();
+            newsModel.setAuthorId((long) 1 + rand.nextInt(31 - 1));
+            res.add(newsModel);
         }
         return res;
+    }
+
+    private static List<String> readFile(String path) {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classloader.getResourceAsStream(path);
+        List<String> result = new ArrayList<>();
+        try (Scanner scanner = new Scanner(inputStream)) {
+            while (scanner.hasNextLine()) {
+                result.add(scanner.nextLine());
+            }
+        }
+        return result;
     }
 }
